b1e3c2994e11e66e9a1552a875dc6161084875ee
karimzakir02 <zakirkarim02@gmail.com>
2021-05-17 17:48:22 +0300

    Entering final stages for Outliers notebook

--- .virtual_documents/Outliers.ipynb.py
+++ .virtual_documents/Outliers.ipynb.py
@@ -1,114 +1,69 @@
+# Importing the required libraries
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
-from sklearn.metrics import mean_squared_error
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 import statsmodels.api as sm
 from scipy.spatial.distance import mahalanobis
 from scipy.stats import chi2
 from sklearn.cluster import DBSCAN
-import warnings
 import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings
 warnings.filterwarnings("ignore")
 
 
+# Loading the dataset and saving its length
 houses = pd.read_csv("houses_to_rent_v2.csv")
+original_length = len(houses)
 houses.head()
 
 
-# Defining the features/target and splitting the data
-features = ["city", "area", "rooms", "bathroom", "parking spaces", "floor", "animal", "furniture"]
-target = ["total (R$)"]
-X = houses[features]
-y = houses[target]
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=10)
-
-
-# Preparing categorical data with One Hot Encoding
-cat_cols = ["city", "animal", "furniture"]
-city_encoder = OneHotEncoder(handle_unknown="ignore", sparse=False)
-
-oh_city_train = pd.DataFrame(city_encoder.fit_transform(X_train[cat_cols]))
-oh_city_test = pd.DataFrame(city_encoder.transform(X_test[cat_cols]))
-
-oh_city_train.index = X_train.index
-oh_city_test.index = X_test.index
-
-X_train.drop(cat_cols, axis=1, inplace=True)
-X_test.drop(cat_cols, axis=1, inplace=True)
-
-X_train = X_train.merge(oh_city_train, left_index=True, right_index=True)
-X_test = X_test.merge(oh_city_test, left_index=True, right_index=True)
-
-
-# Floor has some non-numerical values ('-'), these will be replaced by 0, since I am assuming there are no floors, and the data is not missing
-X_train.replace({"-": 0}, inplace=True)
-X_test.replace({"-": 0}, inplace=True)
-X_train["floor"] = X_train["floor"].astype(int)
-X_test["floor"] = X_test["floor"].astype(int)
+# The floor column has some non-numerical values ('-'), these will be replaced by 0, since I am assuming that '-' indicates a single-story house and 
+# does not indicate missing data
+houses.replace({"-": 0}, inplace=True)
+houses["floor"] = houses["floor"].astype(int)
 # Apart from this, there is no other potentially missing data in the dataset
+houses = houses.select_dtypes(include=np.number)
 
 
-default_model = LinearRegression()
-default_model.fit(X_train, y_train)
-default_predictions = default_model.predict(X_test)
-baseline_mae = mean_absolute_error(default_predictions, y_test)
-baseline_mae
-
-
-houses[target].mean()
-
-
-applicable_cols = ["area", "rooms", "bathroom", "floor", "total (R$)"]
+# Getting the observations which are identified as outliers by the Numeric Outlier Technique
+numeric_cols = houses.select_dtypes(include=np.number).columns.tolist()
 k = 1.5
-num_out_train = X_train.merge(y_train, left_index=True, right_index=True)
 to_remove = {}
-for col in applicable_cols:
-    quantiles = num_out_train[[col]].quantile([0.25, 0.75])
+for col in numeric_cols:
+    quantiles = houses[[col]].quantile([0.25, 0.75])
     q1 = quantiles.iloc[0, 0]
     q3 = quantiles.iloc[1, 0]
     iqr = q3 - q1
     lower_bound = q1 - k*iqr
     upper_bound = q3 + k*iqr
-    records_to_remove = num_out_train[(num_out_train[col] > upper_bound) | (num_out_train[col] < lower_bound)].index
-    to_remove[col] = set(records_to_remove)
+    num_out_outliers_index = houses[(houses[col] > upper_bound) | (houses[col] < lower_bound)].index
+    to_remove[col] = set(num_out_outliers_index)
 
 
+# Determining the amount of observations removed
 combined = set()
 for indexes in to_remove.values():
     combined.update(indexes)
 print("Removed:", len(combined))
-len(combined) / len(X_train) * 100
+len(combined) / original_length * 100
 
 
-num_out_X_train = X_train.drop(list(combined), axis=0)
-num_out_y_train = y_train.drop(list(combined), axis=0)
-num_out_regress = LinearRegression()
-num_out_regress.fit(num_out_X_train, num_out_y_train)
-num_out_predictions = num_out_regress.predict(X_test)
-num_out_mae = mean_absolute_error(num_out_predictions ,y_test)
-num_out_mae
-
-
-baseline_mae - num_out_mae
-
-
-num_out_dropped = num_out_train.loc[list(combined)]
+# Getting a slice of the data containing only outliers for each column
+num_out_outliers = houses.loc[list(combined)]
 custom_groups = {}
 for key, values in to_remove.items():
-    removed_obsv = num_out_dropped.loc[values]
+    removed_obsv = houses.loc[values]
     custom_groups[key] = removed_obsv
 
 
+# Preparing to create a summary table
 summary_table = pd.DataFrame(["mean", "count", "min", "max", "std"], columns=["metric"])
 summary_table.set_index("metric", inplace=True)
 for key in custom_groups.keys():
     summary_table[key] = np.NaN
 
 
+# Populating the summary table
 for key, group in custom_groups.items():
     summary_table.loc["mean", key] = group[key].mean()
     summary_table.loc["count", key] = group[key].count()
@@ -118,100 +73,144 @@ for key, group in custom_groups.items():
 summary_table
 
 
-# Check that this is actually how you're supposed to do it
-sm_model = sm.OLS(y_train, X_train).fit()
-influence = sm_model.get_influence()
-cooks = influence.cooks_distance[0]
-cooks_df = pd.DataFrame(cooks, columns=["cooks"])
-cooks_df.index = X_train.index
-X_train_cooks = X_train.merge(cooks_df, left_index=True, right_index=True)
+# Preparing the quantiles and the IQR for each column
+quantiles_dict = {}
+for col in numeric_cols:
+    quantiles = houses[[col]].quantile([0.25, 0.75])
+    q1 = quantiles.iloc[0, 0]
+    q3 = quantiles.iloc[1, 0]
+    iqr = q3 - q1
+    quantiles_dict[col] = [q1, q3, iqr]
 
 
-cooks_threshold = 4/len(X_train_cooks)
-X_train_cooks_filtered = X_train_cooks[X_train_cooks["cooks"] < cooks_threshold]
-y_train_cooks_filtered = y_train.loc[X_train_cooks_filtered.index]
+# Finding the IQR mutlitplier so that less than 1% of the dataset is removed
+currently_removed = len(combined)
+removed_count = [len(combined)]
+removed_indexes = {}
+k_lst = [k]
+one_percent = int(0.01 * original_length)
+while currently_removed > one_percent:
+    k += 0.5
+    k_lst.append(k)
+    for col in numeric_cols:
+        q1 = quantiles_dict[col][0]
+        q3 = quantiles_dict[col][1]
+        iqr = quantiles_dict[col][2]
+        lower_bound = q1 - k*iqr
+        upper_bound = q3 + k*iqr
+        num_out_outliers_index = houses[(houses[col] > upper_bound) | (houses[col] < lower_bound)].index
+        to_remove[col] = set(num_out_outliers_index)
+    combined = set()
+    for indexes in to_remove.values():
+        combined.update(indexes)
+    removed_indexes[k] = list(combined)
+    currently_removed = len(combined)
+    removed_count.append(currently_removed)
+k
 
 
-# Saving the removed observations for later analysis
-cooks_removed_X = X_train_cooks[X_train_cooks["cooks"] >= cooks_threshold]
-cooks_removed_y = y_train.loc[cooks_removed_X.index]
+# Plotting the number of points identified as outliers against the IQR mutlitpliers tested
+plt.title("Number Points Identified as Outliers with Different IQR Multipliers")
+plt.xlabel("IQR Multiplier")
+plt.ylabel("Number of Outliers")
+sns.scatterplot(x=k_lst, y=removed_count)
 
 
-X_train_cooks_filtered.drop(["cooks"], axis=1, inplace=True)
+# Creating a summary table for the new batch of outliers with the IQR mutltipler being 8
+summary_table = pd.DataFrame(["mean", "count", "min", "max", "std"], columns=["metric"])
+summary_table.set_index("metric", inplace=True)
+k = 8
+for col in numeric_cols:
+    quantiles = houses[[col]].quantile([0.25, 0.75])
+    q1 = quantiles.iloc[0, 0]
+    q3 = quantiles.iloc[1, 0]
+    iqr = q3 - q1
+    lower_bound = q1 - k*iqr
+    upper_bound = q3 + k*iqr
+    num_out_outliers = houses[(houses[col] > upper_bound) | (houses[col] < lower_bound)]
+    summary_table[col] = np.NaN
+    summary_table.loc["mean", col] = num_out_outliers[col].mean()
+    summary_table.loc["count", col] = num_out_outliers[col].count()
+    summary_table.loc["min", col] = num_out_outliers[col].min()
+    summary_table.loc["max", col] = num_out_outliers[col].max()
+    summary_table.loc["std", col] = num_out_outliers[col].std()
+summary_table
 
 
-cooks_model = LinearRegression()
-cooks_model.fit(X_train_cooks_filtered, y_train_cooks_filtered)
-cooks_predictions = cooks_model.predict(X_test)
-cooks_mae = mean_absolute_error(cooks_predictions, y_test)
-cooks_mae
+# Finding the inverse of the covariance matrix and the center
+inv_cov = np.linalg.matrix_power(houses.cov(), -1)
+mean = np.mean(houses)
+mahalanobis_houses = houses.copy().select_dtypes(include=np.number)
+mahalanobis_houses["mahalanobis_distance"] = np.NaN
 
 
-baseline_mae - cooks_mae
+# Adding the distance to the copy of the dataset
+for index, values in mahalanobis_houses.iterrows():
+    values = values[0:10]
+    mahalanobis_houses.loc[index, "mahalanobis_distance"] = mahalanobis(values, mean, inv_cov)**2
 
 
-len(X_train_cooks) - len(X_train_cooks_filtered)
-
-
-cooks_removed_X.merge(cooks_removed_y, left_index=True, right_index=True)
-
-
-mahalanobis_train = X_train.merge(y_train, left_index=True, right_index=True)
-inv_covmat = np.linalg.pinv(mahalanobis_train.cov())
-# I have used pseudo-inverse, since the np library throws a Singular Matrix error when using the standard inverse
-mean = np.mean(mahalanobis_train)
-mahalanobis_train["mahalanobis_distance"] = np.NaN
-
-
-for index, values in mahalanobis_train.iterrows():
-    values = values[0:15]
-    mahalanobis_train.loc[index, "mahalanobis_distance"] = mahalanobis(values, mean, inv_covmat)
-
-
-mahalanobis_threshold = chi2.ppf(0.95, mahalanobis_train.shape[1] - 1)
-mahalanobis_filtered = mahalanobis_train[mahalanobis_train["mahalanobis_distance"] < mahalanobis_threshold]
-mahalanobis_removed = mahalanobis_train[mahalanobis_train["mahalanobis_distance"] >= mahalanobis_threshold]
-
-
-mahalanobis_X_test = mahalanobis_filtered.iloc[:,0:14]
-mahalanobis_y_test = mahalanobis_filtered.iloc[:, 14]
-mahalanobis_model = LinearRegression()
-mahalanobis_model.fit(mahalanobis_X_test, mahalanobis_y_test)
-mahalanobis_predictions = mahalanobis_model.predict(X_test)
-mahalanobis_mae = mean_absolute_error(mahalanobis_predictions, y_test)
-mahalanobis_mae
-
-
-baseline_mae - mahalanobis_mae
-
-
-mahalanobis_mae - cooks_mae
-
-
+# Finding the cut-off and identifying outliers
+mahalanobis_threshold = chi2.ppf(0.99, mahalanobis_houses.shape[1] - 1)
+mahalanobis_removed = mahalanobis_houses[mahalanobis_houses["mahalanobis_distance"] > mahalanobis_threshold].index
 len(mahalanobis_removed)
 
 
-mahalanobis_removed
+# Determing the proportion of the dataset that the outliers take up
+len(mahalanobis_removed) / original_length * 100
 
 
-mahalanobis_threshold_80 = chi2.ppf(0.8, mahalanobis_train.shape[1] - 1)
-mahalanobis_train_80_removed = mahalanobis_train[mahalanobis_train["mahalanobis_distance"] >= mahalanobis_threshold_80]
-mahalanobis_train_80_removed
+# Getting a sample of the outliers
+houses.loc[mahalanobis_removed].sample(n=20, random_state=10)
 
 
-mahalanobis_train_80 = mahalanobis_train[mahalanobis_train["mahalanobis_distance"] < mahalanobis_threshold_80]
-mahalanobis_X_test_80 = mahalanobis_train_80.iloc[:,0:14]
-mahalanobis_y_test_80 = mahalanobis_train_80.iloc[:, 14]
-mahalanobis_model_80 = LinearRegression()
-mahalanobis_model_80.fit(mahalanobis_X_test_80, mahalanobis_y_test_80)
-mahalanobis_predictions_80 = mahalanobis_model_80.predict(X_test)
-mahalanobis_mae_80 = mean_absolute_error(mahalanobis_predictions_80, y_test)
-mahalanobis_mae_80
+houses.loc[mahalanobis_removed].sample(n=20, random_state=20)
 
 
-dbscan_train = X_train.merge(y_train, left_index=True, right_index=True)
+# Preparing our features and targets
+features = houses.columns[0:9]
+target = houses.columns[9]
+X = houses[features]
+y = houses[target]
+
+
+# Training a model and getting Cook's Distance for each point
+X = sm.tools.tools.add_constant(X)
+sm_model = sm.regression.linear_model.OLS(y, X).fit()
+influence = sm_model.get_influence()
+influence_list = influence.cooks_distance[0]
+influence_df = pd.DataFrame(influence_list, columns=["influence"])
+influence_df.index = houses.index
+cooks_df = houses.merge(influence_df, left_index=True, right_index=True)
+
+
+cooks_threshold = 4/original_length
+cooks_outliers = cooks_df[cooks_df["influence"] > cooks_threshold]
+len(cooks_outliers)
+
+
+len(cooks_outliers) / original_length * 100
+
+
+cooks_outliers.sort_values(by=["influence"])
+
+
+filtered_houses = houses[(houses["area"] >= 89) & (houses["area"] <= 99)]
+filtered_houses = filtered_houses[(filtered_houses["rooms"] >= 2) & (filtered_houses["rooms"] <= 4)]
+filtered_houses = filtered_houses[(filtered_houses["bathroom"] >= 1) & (filtered_houses["bathroom"] <= 5)]
+filtered_houses = filtered_houses[(filtered_houses["parking spaces"] >= 1) & (filtered_houses["parking spaces"] <= 3)]
+filtered_houses = filtered_houses[(filtered_houses["hoa (R$)"] >= 700) & (filtered_houses["hoa (R$)"] <= 800)]
+filtered_houses = filtered_houses[(filtered_houses["rent amount (R$)"] >= 2333) & (filtered_houses["rent amount (R$)"] <= 2533)]
+filtered_houses = filtered_houses[(filtered_houses["property tax (R$)"] >= 0) & (filtered_houses["property tax (R$)"] <= 100)]
+filtered_houses = filtered_houses[(filtered_houses["fire insurance (R$)"] >= 21) & (filtered_houses["fire insurance (R$)"] <= 41)]
+filtered_houses.drop(labels=[4994], axis=0)
+
+
+cooks_df.loc[6175]["influence"]
+
+
 dbscan = DBSCAN(eps=0.5, min_samples=5)
-dbscan.fit(dbscan_train)
+dbscan.fit(houses)
 
 
 unique = []
@@ -224,53 +223,36 @@ for value in dbscan.labels_:
 outlier_count
 
 
-outlier_count / len(X_train) * 100
+outlier_count / original_length * 100
 
 
-eps_list = [i for i in np.arange(0.5, 200.5, 1)]
-mae_list = []
-removed_list = []
-for eps in eps_list:
-    dbscan = DBSCAN(eps=eps)
-    dbscan.fit(dbscan_train)
+currently_removed = outlier_count
+eps = 1
+min_samples = 5
+removed_lst = []
+eps_lst = []
+while currently_removed > one_percent:
+    eps_lst.append(eps)
+    dbscan = DBSCAN(eps=eps, min_samples=min_samples)
+    dbscan.fit(houses)
     labels_df = pd.DataFrame(dbscan.labels_, columns=["labels"])
-    labels_df.index = dbscan_train.index
-    current_copy = dbscan_train.copy()
-    current_copy = current_copy.merge(labels_df, right_index=True, left_index=True)
-    removed = len(current_copy[current_copy["labels"] == -1])
-    removed_list.append(removed)
-    current_copy = current_copy[current_copy["labels"] >= 0]
-    dbscan_X = current_copy.iloc[:, :14]
-    dbscan_y = current_copy.iloc[:, 14]
-    dbscan_regress = LinearRegression()
-    dbscan_regress.fit(dbscan_X, dbscan_y)
-    dbscan_predictions = dbscan_regress.predict(X_test)
-    dbscan_mae = mean_absolute_error(dbscan_predictions, y_test)
-    mae_list.append(dbscan_mae)    
+    labels_df.index = houses.index
+    removed_df = labels_df[labels_df["labels"] == -1]
+    removed_indexes = removed_df.index
+    currently_removed = len(removed_indexes)
+    removed_lst.append(currently_removed)
+    eps += 1000
 
 
-fig, ax = plt.subplots(1, 2, figsize=(12,8))
-fig.suptitle("DBSCAN Testing")
-ax[0].set_title("Linear Regression MAE")
-ax[1].set_title("Amount of Observations Removed")
-sns.lineplot(x=eps_list, y=mae_list, ax=ax[0])
-sns.scatterplot(x=eps_list, y=removed_list, ax=ax[1])
+# fig, ax = plt.subplots(1, 2, figsize=(12,8))
+# fig.suptitle("DBSCAN Testing")
+# ax[0].set_title("Linear Regression MAE")
+# ax[1].set_title("Amount of Observations Removed")
+sns.lineplot(x=eps_lst, y=removed_lst)
+# sns.scatterplot(x=eps_list, y=removed_list, ax=ax[1])
 
 
-eps = 50.5
-dbscan_50 = DBSCAN(eps=eps)
-dbscan_50.fit(dbscan_train)
-labels_df_50 = pd.DataFrame(dbscan_50.labels_, columns=["labels"])
-labels_df_50.index = dbscan_train.index
-outliers_index = labels_df_50[labels_df_50["labels"] == -1].index
-outliers_dbscan = dbscan_train.loc[outliers_index]
-len(outliers_dbscan)
 
 
-outliers_dbscan.describe()
 
-
-outliers_dbscan[outliers_dbscan["area"] == 30]
-
-
-outliers_dbscan.sample(15)
+houses.loc[removed_indexes]
